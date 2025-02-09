2343eb5968c2f4698c2d62fbf3b4dc66438d4e61
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-09 13:27:00 +0530

    - kya ho raha hai

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const authRouter = require("./routers/authRouter.js")
 const profileRouter = require("./routers/profile.js")
 const ConnectionRouter = require("./routers/connections.js")
 const dotenv = require("dotenv")
+const User = require("./models/User.js")
 
 app.use(express.json())
 app.use(cookieParser())
@@ -14,6 +15,17 @@ dotenv.config()
 app.use("/test",(req,res)=>{
     res.send("Hello World!!")
 })
+
+app.use("/get",async(req,res)=>{
+    try {
+        const user = await User.findById('67a629603fa94db5d16fc9ef')
+        res.send(user)
+    } catch (error) {
+        res.send(error)
+    }
+})
+
+
 app.use(authRouter)
 app.use(profileRouter)
 app.use(ConnectionRouter)
